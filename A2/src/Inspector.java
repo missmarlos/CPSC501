a90755d78f5de10385d2dc66e91e0a2d2ff66f96
missmarlos <marela.carlos@ucalgary.ca>
2019-10-19 22:30:34 -0600

    1. Declaring class

--- A2/src/Inspector.java
+++ A2/src/Inspector.java
@@ -1,8 +1,18 @@
+import java.lang.reflect.*;
+
 public class Inspector {
 
     public void inspect(Object obj, boolean recursive) {
         Class c = obj.getClass();
         inspectClass(c, obj, recursive, 0);
+    	
+    	//Declaring class
+    	String className = c.getName();
+    	System.out.println("The class name is "+className);
+
+    	//Name of immediate superclass
+    	
+
     }
 
     private void inspectClass(Class c, Object obj, boolean recursive, int depth) {
